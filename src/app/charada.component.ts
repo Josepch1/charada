bbba1134ba00287cd07e77f51c94bc22390afd65
Josepch1 <josepedro.homenhuck@gmail.com>
2022-09-09 18:47:07 -0300

    saas

--- src/app/charada.component.ts
+++ src/app/charada.component.ts
@@ -1,13 +1,17 @@
 import { Component, HostListener } from '@angular/core';
+import { timeout } from 'rxjs';
+import { WORDS } from './wordlist';
 
 const wordLenght = 5;
 const tries = 6;
 
 const letters = (() => {
-  const ret: {[key: string]:boolean} = {}
-
-  //for(let )
-});
+  const ret: {[key: string]: boolean} = {};
+  for (let charCode = 97; charCode < 97 + 26; charCode++) {
+    ret[String.fromCharCode(charCode)] = true;
+  }
+  return ret;
+})();
 
 interface Try {
   letters: Letters[];
@@ -33,6 +37,15 @@ enum LetterState {
 export class Charada {
   readonly tries: Try[] = [];
 
+  msg = '';
+  fadeOutInfoMsg = false;
+
+  private palavraCerta = '';
+
+  private currentLetterIndex = 0;
+
+  private submittedTries = 0;
+
   constructor(){
     for(let i = 0; i < tries; i++){
       const letters: Letters[] = [];
@@ -41,6 +54,18 @@ export class Charada {
       }
       this.tries.push({letters})
     }
+
+    const numeroPalavras = WORDS.length;
+    while(true) {
+      const index = Math.floor(Math.random() * numeroPalavras);
+      const word = WORDS[index];
+
+      if(word.length === wordLenght) {
+        this.palavraCerta = word.toLowerCase();
+        break;
+      }
+    }
+    console.log('PALAVRA CERTA: ', this.palavraCerta);
   }
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
@@ -48,6 +73,52 @@ export class Charada {
   }
 
   private handleClickKey(key: string) {
+    if(letters[key.toLowerCase()]) {
+      if(this.currentLetterIndex < (this.submittedTries + 1) * wordLenght){
+        this.setLetter(key);
+        this.currentLetterIndex++;
+      }
+    }
+    else if(key === 'Backspace'){
+      if (this.currentLetterIndex > this.submittedTries * wordLenght){
+        this.currentLetterIndex--;
+        this.setLetter('');
+      }
+    }
+    else if(key === 'Enter'){
+      this.checkTry();
 
   }
+  }
+
+  private checkTry() {
+    const currentTryIndex = this.tries[this.submittedTries];
+    if (currentTryIndex.letters.some(letter => letter.text === '')) {
+      return;
+    }
+    ///const palavraUsadaCurrentTry = currentTryIndex.letters.map(letter => letter.text).join('').toUpperCase;
+    ///if(!WORDS.includes(palavraUsadaCurrentTry)){
+    ///  return;
+    ///}
+  }
+
+  private showMsg(msg: string) {
+    this.msg = msg;
+
+    setTimeout(() => {
+      this.fadeOutInfoMsg = true;
+      setTimeout(() => {
+        this.msg = '';
+        this.fadeOutInfoMsg = false;
+      }, 500);
+    }, 3000);
+  }
+
+  private setLetter(letter: string){
+    const indexTry = Math.floor(this.currentLetterIndex / wordLenght);
+    const indexLetter = this.currentLetterIndex - indexTry * wordLenght;
+    this.tries[indexTry].letters[indexLetter].text = letter;
+  }
+
+
 }
